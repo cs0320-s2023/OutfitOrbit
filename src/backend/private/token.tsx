bc9232e4dffed22d141011a86792d5929183d1ac
Javier <javier.fergarcia@gmail.com>
2023-04-21 17:40:38 -0400

    ASDAD

--- src/backend/private/token.tsx
+++ src/backend/private/token.tsx
@@ -1,2 +1,2 @@
-let OPEN_API_KEY: string = "sk-vw2sXgfHz7FAvoozy4UBT3BlbkFJOLpozx3OMEXj0JzrKM1";
+let OPEN_API_KEY: string = "sk-vw2sXgfHz7FAvoozy4UBT3BlbkFJOLpozx3OMEXj0JzrKM170";
 export default OPEN_API_KEY;
