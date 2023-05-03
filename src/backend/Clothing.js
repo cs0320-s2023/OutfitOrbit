d183ad75e109ad18c3aefa1a3ac83017744cec0c
taleenac <taleena_chandaria@brown.edu>
2023-05-03 18:25:07 -0400

    parsed

--- src/backend/Clothing.js
+++ src/backend/Clothing.js
@@ -16,4 +16,15 @@ export class Clothing {
         brand: this.brand,
     };
   }
+
+  parseClothingItem(clothingObj) {
+  const type = clothingObj.type;
+  const color = clothingObj.color;
+  const material = clothingObj.material;
+  const occassion = clothingObj.occassion;
+  const brand = clothingObj.brand;
+
+  return new Clothing(type, color, material, occassion, brand);
+}
+
 }
