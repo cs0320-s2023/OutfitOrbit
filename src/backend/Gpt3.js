38232b08a565d436b86ac3378f1f4985534534a8
taleenac <taleena_chandaria@brown.edu>
2023-05-04 11:12:57 -0400

    save

--- src/backend/Gpt3.js
+++ src/backend/Gpt3.js
@@ -3,16 +3,19 @@ import OPEN_API_KEY from "./private/token.tsx";
 
 const configuration = new Configuration({
   apiKey: OPEN_API_KEY,
+  // headers: {
+  //   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+  // }
 });
 
 const openai = new OpenAIApi(configuration);
 
 export default async function Gpt3 (word1) {
+  console.log(openai)
 const completion = await openai.createCompletion({
   model: "text-davinci-003",
   prompt:
-    "Make an outfit from the follow items with this description " + word1 +" and return an object in the same of a" 
-    +"list of objects with fields brand, color, material, occassion and type and only return this object to me",
+    "Make an outfit from the following items:\n\nbrand\n\"zara\"\ncolor\n\"red\"\nmaterial\n\"cotton\"\noccassion\n\"casual\"\ntype\n\"shirt\"\n\nbrand\n\"zara\"\ncolor\n\"red\"\nmaterial\n\"cotton\"\noccassion\n\"casual\"\ntype\n\"pants\"\n\nwith this description " + word1 + " and return an object in the form of a list of objects with fields brand, color, material, occassion, and type, and only return this object to me",
   max_tokens: 3500,
 });
 const text = completion.data.choices[0].text;
--- src/frontend/private/token.tsx
+++ src/frontend/private/token.tsx
@@ -1,2 +1,3 @@
-let OPEN_API_KEY: string = "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";
+let OPEN_API_KEY: string =
+  "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";
 export default OPEN_API_KEY;
